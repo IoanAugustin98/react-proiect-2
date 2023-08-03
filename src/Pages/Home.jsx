a9e9563c906bb2e2f704fc59adc02bf181aef8d6
IoanAugustin98 <simuleac.ioan@yahoo.com>
2023-08-03 20:11:10 +0300

    Bootstrap | Style HomePage + Parse JSON file

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,43 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import HomeCategory from '../components/HomeCategory'
+import products from '../Utils/products.json'
 
-function Home() {
-  return (
-    <div>
-      <Layout>
-        <h1>Home</h1>
-      </Layout>
-    </div>
-  )
+class Home extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      categories:[]
+    }
+  }
+  componentDidMount(){
+    const categories = Object.values(products);
+    this.setState({categories});
+  }
+  render(){
+    return (
+      <div>
+        <Layout>
+          <div className="container">
+            <div className="row">
+              {
+                this.state.categories.map((category,index)=>{
+                  return(
+                    <HomeCategory
+                      key         = {index}
+                      image       = {category.image}
+                      title       = {category.name}
+                      description = {category.description}
+                    />
+                  )
+                })
+              }
+            </div>
+          </div>
+        </Layout>
+      </div>
+    )
+  }
 }
 
 export default Home
